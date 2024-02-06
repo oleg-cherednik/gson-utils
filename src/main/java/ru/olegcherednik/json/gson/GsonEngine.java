f3555236840b207b7e1709c4507566d951095986
Oleg Cherednik <oleg.cherednik@gmail.com>
2024-02-06 14:24:35 +0300

    api

--- src/main/java/ru/olegcherednik/json/gson/GsonEngine.java
+++ src/main/java/ru/olegcherednik/json/gson/GsonEngine.java
@@ -17,7 +17,6 @@
 package ru.olegcherednik.json.gson;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import lombok.RequiredArgsConstructor;
 import ru.olegcherednik.json.api.AutoCloseableIterator;
